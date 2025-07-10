b85537440b18e28f791bd0df964b264c0716410f
mhedlund7 <mhedlund@seas.upenn.edu>
2025-07-10 13:08:08 -0400

    reformatting scene structure

--- frontend/Godot-Multi-Agent-Playground/scenes/test/test_scene_new_house.gd
+++ frontend/Godot-Multi-Agent-Playground/scenes/test/test_scene_new_house.gd
@@ -1,7 +1,15 @@
 extends Node2D
 
 @onready var debugging_input: LineEdit = $UIManager/DebuggingInput
+@onready var ui_manager: CanvasLayer = $UIManager
+
+@onready var http_manager: Node = $HttpManager
+@onready var text_input_manager: Node2D = $TextInputManager
+
+@onready var object_manager: Node2D = $ObjectManager
+@onready var agent_manager: AgentManager = $AgentManager
 @onready var debug_player: DebugPlayer = $DebugPlayer
 
 func _ready():
 	debugging_input.connect("debugging_input_submitted", Callable(debug_player, "_on_debugging_input_submitted"))
+	debugging_input.connect("debugging_input_submitted", Callable(text_input_manager, "_on_debugging_input_submitted"))
---
+++ frontend/Godot-Multi-Agent-Playground/scenes/test/text_input_manager.gd
@@ -0,0 +1,4 @@
+extends Node2D
+
+func _on_debugging_input_submitted(text: String) -> void:
+	pass
